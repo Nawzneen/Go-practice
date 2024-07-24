4c37d4533604730509aa0db05ca4b042f1cec3e2
nawzneen <earworm90@gmail.com>
2024-07-24 13:14:35 +0300

    Feat: add port from .env file

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -15,7 +17,11 @@ type Todo struct {
 
 func main() {
 	app := fiber.New()
-
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
 	todos := []Todo{}
 	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(todos)
@@ -61,5 +67,5 @@ func main() {
 		}
 		return c.Status(400).JSON(fiber.Map{"error": "Todo not found"})
 	})
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(":" + PORT))
 }
